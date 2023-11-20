e87dec720d17f49a4fc4eb6ffd78df19adedfb1e
Erica! <jl6121@columbia.edu>
2023-11-19 20:44:27 -0800

    adjusted the front end layout

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -43,7 +43,7 @@ function Newsfeed() {
           </div>
         </div>
         <div className="newsfeed__buying__section">
-          <h2> Buying Power</h2>
+          <h2> Buying Power:&nbsp;&nbsp;</h2>
           <h2> $4.11</h2>
         </div>
         <div className="newsfeed__market__section">
