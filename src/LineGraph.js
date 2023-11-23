ee7639b486d38aa4175a5817fb25619783c4f4fc
Erica! <jl6121@columbia.edu>
2023-11-23 00:11:55 -0800

    final main page UI

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -74,7 +74,7 @@ function LineGraph({ casesType }) {
             datasets: [
               {
                 type: 'line',
-                backgroundColor: "#1E2023",
+                backgroundColor: "#3F3F44",
                 borderColor: "#5AC53B", 
                 borderWidth: 2,
                 pointBorderColor: 'rgba(0, 0, 0, 0)',
